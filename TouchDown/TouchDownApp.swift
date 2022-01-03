202f94c13451b47dbee3baa59283d56b0c450422
Asif Thaha <asifthaha@gmail.com>
2022-01-03 12:16:06 +0100

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Asif Thaha on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
